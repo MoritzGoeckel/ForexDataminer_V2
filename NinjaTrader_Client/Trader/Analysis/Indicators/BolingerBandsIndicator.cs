bdfa84a8cfc0e55be99baa7929cd371a8e7192de
Moritz Göckel <Moritz Gckel>
2016-10-05 21:02:02 +0900

    Created some indicators

---
+++ NinjaTrader_Client/Trader/Analysis/Indicators/BolingerBandsIndicator.cs
@@ -0,0 +1,62 @@
+﻿using NinjaTrader_Client.Trader.MainAPIs;
+using NinjaTrader_Client.Trader.Model;
+using System;
+using System.Collections.Generic;
+
+namespace NinjaTrader_Client.Trader.Indicators
+{
+    class BolingerBandsIndicator : WalkerIndicator
+    {
+        private long timeframe;
+
+        private MovingAverageIndicator ma;
+        private StandartDeviationIndicator std;
+        private double stdMultiplicator;
+
+        public BolingerBandsIndicator(long timeframe, double stdMultiplicator)
+        {
+            this.stdMultiplicator = stdMultiplicator;
+            this.timeframe = timeframe;
+            this.ma = new MovingAverageIndicator(timeframe);
+            this.std = new StandartDeviationIndicator(timeframe);
+        }
+
+        double valueNow;
+        long timestampNow;
+        public override void setNextData(long _timestamp, double _value)
+        {
+            if (_timestamp < timestampNow)
+                throw new Exception("Cant add older data here!");
+
+            if (_timestamp == timestampNow && _value != valueNow)
+                throw new Exception("Same timestamp different value!");
+
+            if (_timestamp == timestampNow && _value == valueNow)
+                return;
+
+            timestampNow = _timestamp;
+            valueNow = _value;
+
+            ma.setNextData(_timestamp, _value);
+            std.setNextData(_timestamp, _value);
+        }
+
+        public override TimeValueData getIndicator()
+        {
+            double upperBond = (ma.getIndicator().value * valueNow) + (std.getIndicator().value * stdMultiplicator);
+            double lowerBond = (ma.getIndicator().value * valueNow) - (std.getIndicator().value * stdMultiplicator);
+
+            return new TimeValueData(timestampNow, (valueNow - lowerBond) / (upperBond - lowerBond) * 100d);
+        }
+
+        public override string getName()
+        {
+            return "BOLINGERBANDS_" + timeframe + "_" + stdMultiplicator;
+        }
+
+        public override bool isValid(long timestamp)
+        {
+            return ma.isValid(timestamp) && std.isValid(timestamp) && timestamp - timestampNow < 5 * 60; //Last data not older then 5 min
+        }
+    }
+}
---
+++ NinjaTrader_Client/Trader/Analysis/Indicators/MovingAveragePriceCrossoverIndicator.cs
@@ -0,0 +1,52 @@
+﻿using NinjaTrader_Client.Trader.MainAPIs;
+using NinjaTrader_Client.Trader.Model;
+using System;
+using System.Collections.Generic;
+
+namespace NinjaTrader_Client.Trader.Indicators
+{
+    class MovingAveragePriceCrossoverIndicator : WalkerIndicator
+    {
+        private long timeframe;
+        private MovingAverageIndicator ma;
+        public MovingAveragePriceCrossoverIndicator(long timeframe)
+        {
+            ma = new MovingAverageIndicator(timeframe);
+            this.timeframe = timeframe;
+        }
+
+        double valueNow;
+        long timestampNow;
+        public override void setNextData(long _timestamp, double _value)
+        {
+            if (_timestamp < timestampNow)
+                throw new Exception("Cant add older data here!");
+
+            if (_timestamp == timestampNow && _value != valueNow)
+                throw new Exception("Same timestamp different value!");
+
+            if (_timestamp == timestampNow && _value == valueNow)
+                return;
+
+            timestampNow = _timestamp;
+            valueNow = _value;
+
+            ma.setNextData(_timestamp, _value);
+        }
+
+        public override TimeValueData getIndicator()
+        {
+            return new TimeValueData(timestampNow, (ma.getIndicator().value * valueNow) - valueNow);
+        }
+
+        public override string getName()
+        {
+            return "MAPRICECROSS_" + timeframe;
+        }
+
+        public override bool isValid(long timestamp)
+        {
+            return ma.isValid(timestamp) && timestamp - timestampNow < 60 * 5; //Neuster preis nicht älter als 5min
+        }
+    }
+}
